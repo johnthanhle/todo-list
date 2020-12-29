8e9183e4b402c92ef3191b966b1cd3f3731ace75
johnle@berkeley.edu <johnle@berkeley.edu>
2020-12-29 05:44:36 -0800

    UI update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -277,7 +277,7 @@ export default function App() {
                     setIsEdit(true);
                     setID(_id);
                     setEditInput({
-                      task: '',
+                      task: task,
                     });
                   }}
                   >
